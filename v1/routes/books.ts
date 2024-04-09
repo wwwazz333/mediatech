be7e34cc89950ee9e849a1bce5098a552b2c7feb
wwwazz <wwwazz333@gmail.com>
2024-04-09 23:51:49 +0200

    get all & create

--- v1/routes/books.ts
+++ v1/routes/books.ts
@@ -1,9 +1,30 @@
 import { Router } from "express";
+import { bookSchema } from "../models/books";
+import * as bookService from "../services/books";
 const router = Router();
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-	res.send('users');
+
+router.get('/', async function (req, res) {
+	try {
+		const books = await bookService.getAll();
+		res.json(books);
+	}
+	catch (e: any) {
+		console.error("Error getting books", e);
+		res.status(500).send(e.message);
+	}
+});
+
+router.post('/', async function (req, res) {
+	try {
+		const newBook = bookSchema.parse(req.body);
+		await bookService.create(newBook);
+		res.status(201).send();
+	}
+	catch (e: any) {
+		console.error("Error creation book", e.message);
+		res.status(500).send(e.message);
+	}
 });
 
 export default router; 
--- v1/services/books.ts
+++ v1/services/books.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 import { Book, bookSchema } from "../models/books";
 import database from "./database";
 
+const tableName = "Book";
 
-
+///Get all books
 export async function getAll(): Promise<Book[]> {
-	const sql = `SELECT * FROM books`;
+	const sql = `SELECT * FROM ${tableName}`;
 
-	//instead of using a callback, we use a promise
 	const rows = await new Promise((resolve, reject) => {
 		database.all(sql, [], (err, rows) => {
 			if (err) {
@@ -18,18 +18,19 @@ export async function getAll(): Promise<Book[]> {
 		});
 	});
 	try {
+		console.debug("rows", rows)
 		const books = z.array(bookSchema).parse(rows);
 
 		return books;
-	} catch (e) {
-		console.error("Error parsing books");
-		return [];
+	} catch (e: any) {
+		console.error("Error parsing books from BDD", e.message);
+		throw new Error("Error parsing books from BDD");
 	}
 }
 
 //Create a book
 export async function create(book: Book): Promise<void> {
-	const sql = `INSERT INTO books (id, name, description, genre, numberAvailable) VALUES (?, ?, ?, ?, ?)`;
+	const sql = `INSERT INTO ${tableName} (id, name, description, genre, numberAvailable) VALUES (?, ?, ?, ?, ?)`;
 	const params = [book.id, book.name, book.description, book.genre, book.numberAvailable];
 
 	await new Promise((resolve, reject) => {
