26d3c96c236c9c06e8be44065fc7ef083da91ef8
wwwazz <wwwazz333@gmail.com>
2024-04-10 13:43:38 +0200

    search written

--- v1/models/written.ts
+++ v1/models/written.ts
@@ -17,3 +17,21 @@ export const writtenSchema = z.object({
 		return new Date(str)
 	}).or(z.date())
 })
+
+export interface WrittenSearch {
+	idBook?: number | null,
+	idAuthor?: number | null,
+	publication?: Date | null
+}
+
+export const writtenSearchSchema = z.object({
+	idBook: z.number().optional().nullable(),
+	idAuthor: z.number().optional().nullable(),
+	publication: z.string().optional().nullable().transform((str) => {
+		if (str) {
+			return new Date(str)
+		} else {
+			return null;
+		}
+	}).or(z.date().optional().nullable())
+})
